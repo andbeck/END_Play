be46ed884a09cf18c85426fe2d3bcd2a0201bf2b
Andrew Beckerman <andrew.beckerman@gmail.com>
2023-06-10 10:34:02 +0100

    processing functions practice

--- # How to use processing functions.jl
+++ # How to use processing functions.jl
@@ -4,3 +4,28 @@ using Plots
 using Random, DataFrames, StatsPlots
 using EcologicalNetworks, EcologicalNetworksPlots
 using EcologicalNetworksDynamics
+
+Random.seed!(123)
+S = 10
+A = FoodWeb(nichemodel, S; C = 0.1)
+params = ModelParameters(A)
+bo = rand(S)
+out = simulate(params, bo)
+
+plot(out)
+
+
+# biomass
+biomass(out)
+biomass(out).total
+
+# diversity
+EcologicalNetworksDynamics.richness(out)
+species_persistence(out)
+evenness(out)
+shannon_diversity(out)
+
+# stability
+coefficient_of_variation(out)
+community_cv(out)
+species_cv(out)
