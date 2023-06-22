c1e6ce342a13c4b864d228a1c5001b72f692ab4d
andbeck <andrew.beckerman@gmail.com>
2023-06-22 15:26:53 +0200

    create 2 resource competition coexistence

--- # Eva Use Case APB.jl
+++ # Eva Use Case APB.jl
@@ -9,6 +9,106 @@ using DataFrames, Distributions
 using EcologicalNetworksDynamics
 
 using StatsPlots
+import Random
+
+#### bespoke testing -----
+tmax = 10000
+
+# 3 species using Brose 2008 paramter means
+foodweb = FoodWeb([0 0;0 0]); # two competitors
+
+# two resources half saturations
+k11 = 0.3
+k12 = 0.9
+k21 = 0.9
+k22 = 0.3
+
+# turnover rate
+D = 0.9 #round(rand(Uniform(0.1, 0.4), 1)[1]; digits = 2)
+
+# supply rate
+sr = 10
+
+# extra equation parameters
+d_exclude = 0.25 # mortality for exclusion
+d_coexist = 0.5 # mortality for coexist
+r_exclude = [0.28, 0.3]
+r_coexist = [1,1]
+
+
+# growth model for exclusion
+# one resource
+growthmodel_exclusion = NutrientIntake(
+    foodweb;
+    n_nutrients = 1,
+    supply = sr,
+    half_saturation = [k11, k12],
+    turnover = D,
+    concentration = 1
+)
+
+# growth model for coexistence
+# two resources
+growthmodel_coexist = NutrientIntake(
+    foodweb;
+    n_nutrients = 2,
+    supply = sr,
+    half_saturation = [k11 k12 ; k21 k22],
+    turnover = D,
+    concentration = [1,1]
+)
+
+
+params_exclude =
+    ModelParameters(foodweb; producer_growth=growthmodel_exclusion,
+    biorates = BioRates(foodweb; d = d_exclude, r = r_exclude))
+
+# no growth model - leads to extinction
+params_coexist =
+    ModelParameters(foodweb; producer_growth = growthmodel_coexist,
+    biorates = BioRates(foodweb; d = d_coexist, r = r_coexist))
+
+# intial biomass and concentrations
+Random.seed!(123)
+B0 = [0.5, 0.5] # Inital plant biomass.
+N0_exclude = 1 .+ 3 * rand(1) # Initial nutrient concentration.
+N0_coexist = 1 .+ 3 * rand(2) # Initial nutrient concentration.
+
+solution_exclude = simulate(
+    params_exclude,
+    B0;
+    N0 = N0_exclude,
+    tmax,
+    alg_hints=[:stiff],
+    reltol=1e-5
+)
+
+solution_coexist = simulate(
+    params_coexist,
+    B0;
+    N0 = N0_coexist,
+    tmax,
+    alg_hints=[:stiff],
+    reltol=1e-5
+)
+
+l = @layout [a b]
+
+p1 = Plots.plot(solution_exclude, label = ["Plant 1" "Plant 2" "R1";],
+    ylim = (0,10))
+title!("Exclusion (1 resource)")
+
+p2 =Plots.plot(solution_coexist,
+    label = ["Plant 1" "Plant 2" "R1" "R2";],
+    ylim = (0, 10))
+title!("Coexistence (2 resources)")
+
+
+Plots.plot(p1, p2, layout = l)
+
+
+
+## ----
 
 """
 Compute biomass extrema for each species during the `last` time steps.
@@ -101,77 +201,3 @@ Legend(
     tellwidth = false, # Do not adjust width of the orbit diagram.
 )
 # save("/tmp/plot.png", fig; resolution = (450, 350), px_per_unit = 3)
-
-#### bespoke testing -----
-
-# 3 species
-foodweb = FoodWeb([3 => [1,2]]); # 3 eats 1 and 2
-
-functional_response = ClassicResponse(foodweb; aᵣ = 1, hₜ = 1, h = 1);
-
-S_values = LinRange(0, 70, 20)
-tmax = 10_000 # Simulation length.
-verbose = false # Do not show '@info' messages during the simulation.
-
-# choose an supply rate
-s = S_values[5]
-
-# three resources
-k1 = 0.1 #round(rand(Uniform(0.1, 0.2), 1)[1]; digits = 2)
-k2 = 0.1 #round(rand(Uniform(0.1, 0.2), 1)[1]; digits = 2)
-k3 = 0.1 #round(rand(Uniform(0.1, 0.2), 1)[1]; digits = 2)
-
-d = 0.1 #round(rand(Uniform(0.1, 0.4), 1)[1]; digits = 2)
-
-# with growth model - leads to coexistence
-growthmodel = NutrientIntake(
-    foodweb;
-    n_nutrients = 2,
-    supply = s,
-    half_saturation = [k1 k2; k1 k2],
-    turnover = d,
-)
-
-
-params =
-    ModelParameters(foodweb; functional_response, producer_growth=growthmodel)
-callback = ExtinctionCallback(1e-6, params, verbose)
-
-# no growth model - leads to extinction
-params2 =
-    ModelParameters(foodweb; functional_response)
-
-B0 = 1 .+ 3 * rand(3) # Inital biomass.
-N0 = 1 .+ 3 * rand(2) # Initial nutrient abundances.
-
-solution = simulate(
-    params,
-    B0;
-    N0,
-    tmax,
-    verbose,
-    alg_hints=[:stiff],
-    callback,
-    reltol=1e-5
-)
-
-solution2 = simulate(
-    params2,
-    B0;
-    N0,
-    tmax,
-    verbose,
-    alg_hints=[:stiff],
-    callback,
-    reltol=1e-5
-)
-
-l = @layout [a b]
-
-p1 =Plots.plot(solution, label = ["Plant 1" "Plant 2" "Consumer" "R1" "R2";])
-xlims!(0,100)
-
-p2 = Plots.plot(solution2, label = ["Plant 1" "Plant 2" "Consumer";])
-xlims!(0,100)
-
-Plots.plot(p1, p2, layout = l)
