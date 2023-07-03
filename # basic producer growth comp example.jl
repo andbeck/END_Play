1e4eb360cd9992aa08eb69de74269d16ddd18387
andbeck <andrew.beckerman@gmail.com>
2023-07-03 15:30:47 +0100

    basic usage update

--- # basic producer growth comp example.jl
+++ # basic producer growth comp example.jl
@@ -6,18 +6,31 @@ Random.seed!(123)
 
 S = 20
 
-foodweb = FoodWeb(nichemodel(S, 0.05))
+# make a food web
+# richness, connectance and PPMR
+foodweb = FoodWeb(nichemodel, S; C = 0.1, Z = 10)
 
-producer_growth_competition = LogisticGrowth(foodweb; K = 10, a = (diag = 1, offdiag = 0.8))
+# set K and producer competition details
+producer_growth_competition = LogisticGrowth(foodweb;
+    K = 10,
+    a = (diag = 1, offdiag = 0.8))
+
+# define the functional response and parameters
 fr = BioenergeticResponse(foodweb; h = 1.2)
 
+# set up the model
 params = ModelParameters(foodweb;
     functional_response = fr,
     producer_growth = producer_growth_competition)
 
+# define initial biomass
 B0 = rand(S) # Initial biomass.
+
+# simulate
 solution = simulate(params, B0)
 
+# find extinctions
 get_extinct_species(solution)
 
+# plot/visualise
 plot(solution)
