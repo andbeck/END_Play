ff412964f84c3187a1f9431fc2c6dfd2f418969a
andbeck <andrew.beckerman@gmail.com>
2023-08-05 15:10:30 +0100

    Binzer New by apb and start continuous temp

---
+++ ContinuousTempSim_GriffithsNew.jl
@@ -0,0 +1,73 @@
+using EcologicalNetworksDynamics
+using Random, DataFrames
+
+# vectors of variables
+T_range = 0:2:40
+K_range = 1.:1:20
+n_rep = 10
+T_values = 273.15 .+ collect(T_range) # temperature 1-40C
+K_int_values = collect(K_range) # intercept of the carrying capacity (eutrophication)
+n_T, n_K = length(T_range), length(K_range)
+
+# basal species starting biomass
+m0 = 0.01
+
+# make 10 food web using niche model and Z value 100
+seed!(22)
+FWs = []
+for _ in 1:n_rep
+    fw = FoodWeb(nichemodel, 30; C=0.1, Z=100)
+    fw.M *= m0
+    push!(FWs, fw)
+end
+
+
+# dataframe to store results
+n_lines = n_T * n_K * n_rep
+df = DataFrame(step = [], temp=[], richness = [])
+
+# start model
+foodweb = FWs[1]
+p = ModelParameters(foodweb, functional_response=ClassicResponse(foodweb, h=1.2), biorates=BioRates(foodweb; d=0))
+# set initial biomasses
+B0 = zeros(richness(foodweb))
+K_prod = unique(p.producer_growth.K[.!isnothing.(p.producer_growth.K)])
+B0[producers(foodweb)] .= K_prod
+B0[1:richness(foodweb) .∉ [producers(foodweb)]] .= K_prod / 8
+
+
+#for i in 1:10
+    TT = T_values[i]
+    KK = 1
+    K_int = K_int_values[1]
+    K_prod = unique(p.producer_growth.K[.!isnothing.(p.producer_growth.K)])
+
+    # set temperature
+    set_temperature!(p, TT, ExponentialBA(K = exp_ba_carrying_capacity(aₚ = K_int)))
+
+    # simulate biomass dynamics for 10 years
+    out = simulate(p, B0 , tmax = 3153600000,
+        callback = EcologicalNetworksDynamics.ExtinctionCallback(1e-12, p, false),
+        adaptive = true,
+        dt = 24*60*60,
+        saveat = 24*60*60,
+        )
+
+    richness = richness(out)
+    push!(df,step = i, temp = TT, richness = richness)
+
+    # update Biomass
+    B0 = biomass(out, last = 1)
+
+    # update foodweb
+    fw_updated = deepcopy(foodweb)
+    extinctions = get_extinct_species(out)
+    whichDel = keys(extinctions)
+
+    species = 1:size(fw_updated.A,1)
+    species_keep = species .!= whichDel
+
+    A_updated = fw_updated.A[species_keep, species_keep]
+    M_updated = fw_updated.M[species_keep]
+
+    p = ModelParameters(fw_updated, functional_response=ClassicResponse(fw_updated, h=1.2), biorates=BioRates(fw_updated; d=0))
--- TempBinzerCoarsePlot.R
+++ TempBinzerCoarsePlot.R
@@ -10,15 +10,24 @@ df100_2 <-read_csv("Binzer_2016_z100_finer.csv")
 col = rev(RColorBrewer::brewer.pal(9, "Greens"))
 
 p1 <- ggplot(df10, aes(x = eutrophication, y = temp, fill = persistence))+
-  geom_tile()+
+  geom_tile()+ggtitle("10")+
   scale_fill_gradientn(colours = col)
 
 p2 <- ggplot(df100, aes(x = eutrophication, y = temp, fill = persistence))+
-  geom_tile()+
+  geom_tile()+ggtitle("100")+
   scale_fill_gradientn(colours = col)
+
+p1+p2
 
 p3 <- ggplot(df10_2, aes(x = eutrophication, y = temp-273.5, fill = persistence))+
   geom_tile()+
+  labs(y = "Temperature C˚", x = "Productivity", title = "10")+
   scale_fill_gradientn(colours = col)
 
-p3
+p4 <- ggplot(df100_2, aes(x = eutrophication, y = temp-273.5, fill = persistence))+
+  geom_tile()+
+  labs(y = "Temperature C˚", x = "Productivity", title = "100")+
+  scale_fill_gradientn(colours = col)
+
+p3 + p4
+
