41ca6bf7fd216fd5be3d423dda6106bf8e2c9d28
andbeck <andrew.beckerman@gmail.com>
2024-01-11 16:28:44 +0000

    messing with Trophic Levels

--- TempBinzerCoarsePlot.R
+++ TempBinzerCoarsePlot.R
@@ -21,8 +21,11 @@ p0 <- ggplot(basic, aes(x = temp-273.15, y = meanPersistence,
                         group = eutrophication, col = factor(eutrophication)))+
   geom_point()+
   geom_line()+
-  geom_errorbar(aes(ymin = meanPersistence - sePersistence,
-                    ymax = meanPersistence + sePersistence), alpha = 0.5)
+  labs(x = "Temperature (C)", y = "Mean Persistence (n = 10)")+
+  # geom_errorbar(aes(ymin = meanPersistence - sePersistence,
+  #                   ymax = meanPersistence + sePersistence), alpha = 0.5)
+  theme_bw(base_size = 15)+
+  theme(legend.title = element_blank())
 
 p0
 
