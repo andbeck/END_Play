e3ee56fc6697698a82b994f65d5e7f169ef5ed1e
andbeck <andrew.beckerman@gmail.com>
2024-03-26 13:00:13 +0000

    update for Ethan

--- Start_Stop_dev.jl
+++ Start_Stop_dev.jl
@@ -7,6 +7,7 @@ using DataFrames
 # # 5% connectance and PPMR of 10x
 # foodweb = FoodWeb(nichemodel, 20, C = 0.05, Z = 10)
 
+# The parrot community food web
 foodweb = FoodWeb(
 [6 => [1,2,3,4,5],
 7 => [1,2,3,4,5,6],
@@ -20,89 +21,115 @@ foodweb = FoodWeb(
 15 => [7,8,9,10,11,12,13,14]], Z = 10)
 
 #randomise starter
-Random.seed!(123)
+Random.seed!(1234)
 
 # INTIAL SETUP
 # set the biomasses of the 20 species
-# parrot is 12?
+# parrot is 8?
+
 B0 = rand(15)
 
 # set up the model parameters
+# can add Mass (body size) details here
 # could do more here with the functional response etc
+# (e.g. default is a type 3 functional response, which is very stable)
+# (what happens when it's a type 2 and less stable)
+
 params = ModelParameters(foodweb)
 
-
 ## Collection Zone - set up data frame to collect richness, biomass and stability
-df = DataFrame(richness = [], biomass = [], stability = [], Sp12_Biomass = [])
+df = DataFrame(richness = [], biomass = [], stability = [], Sp8_Biomass = [])
+
+# collect all species biomasses at end of each simulation
+all_biomass = Any[]
 
 ## Looping
 
+# 10 harvesting events
 for i in 1:10
 
-    # simulate -> i = 1 is burn-in
+    # just some information during the simulations
+    B8_start = B0[8]
+    @info "Iteration number: $i"
+    @info "Biomass Sp 8 init: $B8_start"
+
+    # simulate
     out = simulate(params, B0, verbose = false)
 
     # collect data for each cycle
     push!(df, [richness(out), biomass(out).total, coefficient_of_variation(out).community,
-        B0[12]])
-
-    #############################################
-    ### identify extinctions in prep for i+1 ####
-    #############################################
-
-    # who is extinct
-    who_extinct = keys(get_extinct_species(out))
-
-    # a list of 1:n species in current network
-    species = 1:size(foodweb.A, 1)
-
-    # mask extinct species or leave as is if who_extinct is empty
-    if !isempty(who_extinct)
-        species_idx = setdiff(species, who_extinct) # or findall(x->x ∉ who_extinct, species)
-    else
-        species_idx = 1:size(foodweb.A, 1)
-    end
-
-
-    @info "Iteration number: $i"
-    @info "Extinction: $who_extinct"
-    B12 = B0[12]
-    @info "Biomass Sp 12: $B12"
+        B0[8]])
+    push!(all_biomass, B0)
 
     ######################################
-    ## Update foodweb, B0 and p for t+1 ##
+    ## NEW: Update Food Web with Zero's for extinct species
+    ## Retains structure and indexing for plotting
     ######################################
 
-    # subset the matrix
-    foodweb = FoodWeb(foodweb.A[species_idx, species_idx])
+    # grab the trophic structure
+    troph_struc = trophic_structure(out, last = 1)
 
-    # subset and collect the biomass (last value approach vs. mean?)
-    # whether the mean or the last value is taken doesn't seem to matter
-    B0 = biomass(out, last = 1).species[species_idx]
+    # grab the alive species
+    alive_species = troph_struc.alive_species
 
-    #### reduce 12th species biomass by 10% ####
-    #### doesn't actually work, as species 12 is not the same after extinctions
-    #### how would you manage this?
+    # pre-allocate new biomass with all zeros
+    B0 = zeros(size(foodweb.A)[1])
 
-    B0[12] = B0[12]*0.90
+    # replace with biomass of alive species at end of sim
+    # any that are extinct are assigned a 0.
+    B0[alive_species] = biomass(out, last = 1).species[alive_species]
+
+    # just some information during the simulations
+    B8_end = B0[8]
+    @info "Biomass Sp 8 end: $B8_end"
+
+
+    # harvest parrot at 10%
+    B0[8] = B0[8]*0.9
+
+    # just some information during the simulations
+    B8_harvest = B0[8]
+    @info "Biomass Sp 8 harvest: $B8_harvest"
 
     # reset the params and bodymass vector with subsetted bodymass
+    # note no change to the food web structure as we simply mark
+    # extinct ones as 0 biomass
     params = ModelParameters(foodweb)
+
 end
 
-# see the data
+# see the data frame
 df
-df[!,:Sp12_Biomass]
+
+# see parrot trajectory
+df[!,:Sp8_Biomass]
+
+## convert all the B0 vectors (all species) to a data frame
+mat = zeros(length(all_biomass), length(all_biomass[1])) # create a matrix the right size
+
+# stick the data in the right place
+for i in 1:length(all_biomass)
+    mat[i,:] = all_biomass[i]
+end
+
+# see the matrix
+# each column is a species biomass through time.
+mat
+
+# OR classy!
+#DataFrame(mapreduce(permutedims, vcat, all_biomass[:,:]), :auto)
 
 # plotting
 step = 1:10
-a = plot(step, df[!,:Sp12_Biomass])
-title!("Species12")
+a = plot(step, df[!,:Sp8_Biomass])
+title!("Species8")
 b = plot(step, df[!,:biomass])
 title!("Biomass")
 c = plot(step, df[!,:stability])
 title!("Stability")
 d = plot(step, df[!,:richness])
 title!("Richness")
+e = plot(step, mat, legend = false)
+title!("All Species ~ Time")
 
-plot(a,b,c,d, layout=(2,2))
+plot(a,b,c,d,e, layout=(2,3))
